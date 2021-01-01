52c42d40b5fab3b689b2594fafd2a1cd7792b386
grajk88 <30874855+grajk88@users.noreply.github.com>
2021-01-01 20:00:04 +0000

    Added Working Code with Connections Closed

--- src/test/java/com/gt/tests/FilloTest.java
+++ src/test/java/com/gt/tests/FilloTest.java
@@ -1,8 +1,12 @@
 package com.gt.tests;
 
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.codoid.products.exception.FilloException;
 import com.codoid.products.fillo.Connection;
 import com.codoid.products.fillo.Fillo;
@@ -12,12 +16,17 @@ public class FilloTest {
 
 	Connection connection;
 	Fillo fillo = new Fillo();
+	ExtentReports extent = new ExtentReports();
+	ExtentSparkReporter spark = new ExtentSparkReporter("reports/Report.html");
 
 	@BeforeTest
 	public void beforeTest() {
+
 		try {
 
 			connection = fillo.getConnection(System.getProperty("user.dir") + "//data//Name_Details.xlsx");
+
+			extent.attachReporter(spark);
 
 		} catch (FilloException e) {
 
@@ -25,7 +34,7 @@ public class FilloTest {
 		}
 	}
 
-	@Test
+	@Test(enabled = true)
 	public void extractData() {
 
 		try {
@@ -40,7 +49,8 @@ public class FilloTest {
 			}
 
 			recordset.close();
-			connection.close();
+
+			extent.createTest("Extract Data").log(Status.FAIL, "Yay! Passed");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -53,11 +63,11 @@ public class FilloTest {
 
 		try {
 
-			String strQuery = "Update Sheet1 Set Age=35 where ID=1 and name='Giri'";
+			String strQuery = "Update Sheet1 Set Result='PASS' where name='Vaishnavi'";
 
 			connection.executeUpdate(strQuery);
 
-			connection.close();
+			extent.createTest("Write Data").log(Status.PASS, "Yay! Passed");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -69,11 +79,11 @@ public class FilloTest {
 
 		try {
 
-			String strQuery = "INSERT INTO Sheet1(Name,Age,Sex) VALUES('Peter','50','M')";
+			String strQuery = "INSERT INTO Sheet1(Name,Age,Sex) VALUES('Mickey','100','T')";
 
 			connection.executeUpdate(strQuery);
 
-			connection.close();
+			extent.createTest("Insert Data").log(Status.PASS, "Yay! Passed");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -89,11 +99,18 @@ public class FilloTest {
 
 			connection.executeUpdate(strQuery);
 
-			connection.close();
+			extent.createTest("Remove Data").log(Status.PASS, "Yay! Passed");
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		connection.close();
+
+		extent.flush();
 	}
 
 }
